3bc0b1edacc439ddb363b202de6b7d62a03cf872
tapeds <frrllmatthew@gmail.com>
2024-09-24 12:04:28 +0700

    intial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tapeds/go-template/cmd"
+	"github.com/tapeds/go-template/config"
+	"github.com/tapeds/go-template/controller"
+	"github.com/tapeds/go-template/middleware"
+	"github.com/tapeds/go-template/repository"
+	"github.com/tapeds/go-template/routes"
+	"github.com/tapeds/go-template/service"
+)
+
+func main() {
+	db := config.SetUpDatabaseConnection()
+	defer config.CloseDatabaseConnection(db)
+
+	if len(os.Args) > 1 {
+		cmd.Commands(db)
+		return
+	}
+
+	var (
+		jwtService service.JWTService = service.NewJWTService()
+
+		// Implementation Dependency Injection
+		// Repository
+		userRepository repository.UserRepository = repository.NewUserRepository(db)
+
+		// Service
+		userService service.UserService = service.NewUserService(userRepository, jwtService)
+
+		// Controller
+		userController controller.UserController = controller.NewUserController(userService)
+	)
+
+	server := gin.Default()
+	server.Use(middleware.CORSMiddleware())
+
+	// routes
+	routes.User(server, userController, jwtService)
+
+	server.Static("/assets", "./assets")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8888"
+	}
+
+	var serve string
+	if os.Getenv("APP_ENV") == "localhost" {
+		serve = "127.0.0.1:" + port
+	} else {
+		serve = ":" + port
+	}
+
+	if err := server.Run(serve); err != nil {
+		log.Fatalf("error running server: %v", err)
+	}
+}
---
+++ routes/user_route.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tapeds/go-template/controller"
+	"github.com/tapeds/go-template/middleware"
+	"github.com/tapeds/go-template/service"
+)
+
+func User(route *gin.Engine, userController controller.UserController, jwtService service.JWTService) {
+	routes := route.Group("/api/user")
+	{
+		// User
+		routes.POST("", userController.Register)
+		routes.GET("", userController.GetAllUser)
+		routes.POST("/login", userController.Login)
+		routes.DELETE("", middleware.Authenticate(jwtService), userController.Delete)
+		routes.PATCH("", middleware.Authenticate(jwtService), userController.Update)
+		routes.GET("/me", middleware.Authenticate(jwtService), userController.Me)
+		routes.POST("/verify_email", userController.VerifyEmail)
+		routes.POST("/send_verification_email", userController.SendVerificationEmail)
+	}
+}
